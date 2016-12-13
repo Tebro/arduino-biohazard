c42acbf13cfb04f737e972c044cb8d5c9edb3652
Richard Weber <riche.weber@gmail.com>
2016-12-13 16:10:57 +0200

    Init

---
+++ biohazard.ino
@@ -0,0 +1,36 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+const int switchPin = 6;
+int switchState = 0;
+
+void setAllClear(){
+    lcd.clear();
+    lcd.setCursor(7, 0);
+    lcd.print("ALL");
+    lcd.setCursor(6, 1);
+    lcd.print("CLEAR");
+}
+
+void setBioHazard(){
+  lcd.clear();
+  lcd.setCursor(4, 0);
+  lcd.print("WARNING!");
+  lcd.setCursor(3, 1);
+  lcd.print("BIOHAZARD!");
+}
+
+void setup() {
+  lcd.begin(16, 2);
+  pinMode(switchPin, INPUT);
+  setAllClear();
+}
+
+void loop() {
+  switchState = digitalRead(switchPin);
+  if( switchState == HIGH) {
+    setBioHazard();
+    delay(5000);
+    setAllClear();
+  }
+}
